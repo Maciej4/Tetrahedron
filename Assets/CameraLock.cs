cdf46c77b4177840326429bf9cd23799916bfcb6
Maciej4 <m@maciej.me>
2018-10-19 22:16:07 +0100

    Camera Changes

--- Assets/CameraLock.cs
+++ Assets/CameraLock.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLock : MonoBehaviour {
+	//public GameObject target;
+	//public GameObject target;
+	//private Script other;
+	public GameObject g;
 
-	// Use this for initialization
-	void Start () {
+	COMCalculater other = g.GetComponent<COMCalculater>();
 
-	}
+  void Start() {
+		//other = GameObject.GetComponent("COMCalculator");
+  }
 
 	// Update is called once per frame
 	void Update () {
+		transform.position = new Vector3(other.comX()-10, other.comY()+10, other.comZ()-10);
 		transform.eulerAngles = new Vector3(30, -45, 0);
 	}
 }
