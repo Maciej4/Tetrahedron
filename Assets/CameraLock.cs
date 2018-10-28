89e207344fad858f8ccd484f0ab9d0ba93b51072
Maciej4 <m@maciej.me>
2018-10-27 17:16:49 -0700

    add camera control

--- Assets/CameraLock.cs
+++ Assets/CameraLock.cs
@@ -3,29 +3,138 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLock : MonoBehaviour {
-	public GameObject alpha;
-	public GameObject beta;
-	public GameObject charlie;
-	public GameObject delta;
 
-	public float comX;
-	public float comY;
-	public float comZ;
+    public float currentX = 0.0f;
+    public float currentY = 2.0f;
+    public float currentZ = 0.0f;
 
-  void Start() {
+    public float angleX = 90;
+    public float angleY = 0;
+    public float angleZ = 0;
 
-  }
+    public float currentXoffset = 0.0f;
+    public float currentZoffset = 0.0f;
+    public float angleXoffset = 0.0f;
+    public float angleZoffset = 0.0f;
 
-	// Update is called once per frame
-	void Update () {
-		comX = (alpha.transform.position.x+beta.transform.position.x
-		+charlie.transform.position.x+delta.transform.position.x)/4;
-		comY = (alpha.transform.position.y+beta.transform.position.y
-		+charlie.transform.position.y+delta.transform.position.y)/4;
-		comZ = (alpha.transform.position.z+beta.transform.position.z
-		+charlie.transform.position.z+delta.transform.position.z)/4;
+    public float startMouseX;
+    public float startMouseY;
 
-		transform.position = new Vector3(comX+0.3f, comY+0.3f, comZ+0.3f);
-		transform.eulerAngles = new Vector3(30, 225, 0);
-	}
+    public float mouseX;
+    public float mouseY;
+    public float scrollY;
+    public float factor;
+
+    public bool button3;
+    public bool spaceBar;
+
+    void Start() {
+
+    }
+
+    private float scrollFactor(float inputVal) {
+        return (1.87f / inputVal) + 0.08f;
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        
+        if (!spaceBar)
+        {
+            factor = scrollFactor(currentY);
+            mouseX = e.mousePosition.x / (200f * scrollFactor(currentY));
+            mouseY = e.mousePosition.y / (200f * scrollFactor(currentY));
+        }
+        else if (spaceBar)
+        {
+            mouseX = e.mousePosition.x / 50f;
+            mouseY = e.mousePosition.y / 50f;
+        }
+        
+        scrollY = Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    private void panUpdate()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            button3 = true;
+            startMouseX = mouseX;
+            startMouseY = mouseY;
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            button3 = false;
+            currentX -= currentXoffset;
+            currentZ += currentZoffset;
+            currentXoffset = 0.0f;
+            currentZoffset = 0.0f;
+        }
+
+        if (button3)
+        {
+            currentXoffset = mouseX - startMouseX;
+            currentZoffset = mouseY - startMouseY;
+        }
+
+        currentY += 2 * scrollY;
+        currentY = Mathf.Clamp(currentY, 0.1f, 10f);
+
+        transform.position = new Vector3(currentX - currentXoffset, currentY, currentZ + currentZoffset);
+        transform.eulerAngles = new Vector3(90, 0, 0);
+    }
+
+    private void spaceUpdate() {
+        if (Input.GetMouseButtonDown(2))
+        {
+            button3 = true;
+            startMouseX = mouseX*10;
+            startMouseY = mouseY*10;
+        }
+        else if (Input.GetMouseButtonUp(2))
+        {
+            button3 = false;
+            angleX -= angleXoffset;
+            angleZ -= angleZoffset;
+            angleXoffset = 0.0f;
+            angleZoffset = 0.0f;
+        }
+
+        if (button3)
+        {
+            angleXoffset = mouseX*10 - startMouseX;
+            angleZoffset = mouseY*10 - startMouseY;
+            angleZoffset = Mathf.Clamp(angleZoffset, -70f+angleZ, 30f+angleZ);
+        }
+
+        currentY += 2 * scrollY;
+        currentY = Mathf.Clamp(currentY, 0.1f, 10f);
+        angleZ = Mathf.Clamp(angleZ, -30f, 70f);
+
+        float x = currentX + currentY * Mathf.Cos(((angleX-angleXoffset)*3.14f)/180.0f);
+        float z = currentZ + currentY * Mathf.Sin(((angleX-angleXoffset)*3.14f)/180.0f);
+        float distance = Vector3.Distance(new Vector3(x, 0f, z), new Vector3(currentX, 0f, currentZ));
+        float y = currentY + distance*Mathf.Tan(((angleZ-angleZoffset)*3.14f)/180.0f);
+        
+        transform.position = new Vector3(x, y, z);
+        transform.LookAt(new Vector3(currentX,0.1f,currentZ));
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            spaceBar = !spaceBar;
+            angleX = -90.0f;
+            angleZ = 70.0f;
+        }
+
+        if (!spaceBar)
+        {
+            panUpdate();
+        }
+        else if (spaceBar)
+        {
+            spaceUpdate();
+        }
+    }
 }
--- Assets/PointControl.cs
+++ Assets/PointControl.cs
@@ -8,12 +8,30 @@ public class PointControl : MonoBehaviour {
 	public Transform charlie;
 	public Transform delta;
 
-	public float side0 = 0.1f;
-	public float side1 = 0.1f;
-	public float side2 = 0.1f;
-	public float side3 = 0.1f;
-	public float side4 = 0.1f;
-	public float side5 = 0.1f;
+    public float side0set = 0.0f;
+    public float side1set = 0.0f;
+    public float side2set = 0.0f;
+    public float side3set = 0.0f;
+    public float side4set = 0.0f;
+    public float side5set = 0.0f;
+
+    private float side0vel = 0.0f;
+    private float side1vel = 0.0f;
+    private float side2vel = 0.0f;
+    private float side3vel = 0.0f;
+    private float side4vel = 0.0f;
+    private float side5vel = 0.0f;
+
+    public float side0 = 1.0f;
+	public float side1 = 1.0f;
+	public float side2 = 1.0f;
+	public float side3 = 1.0f;
+	public float side4 = 1.0f;
+	public float side5 = 1.0f;
+
+    private float a = 0.9f;
+    private float b = 1.0f;
+    private float moveSpeed = 0.1f;
 
     public class Tetrahedron {
         public Vector3 A { get; set; }
@@ -57,7 +75,21 @@ public class PointControl : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Tetrahedron t = edge_input(side0, side1, side2, side3, side4, side5);
+        side0 = Mathf.SmoothDamp(side0, side0set + b, ref side0vel, 1.0f);
+        side1 = Mathf.SmoothDamp(side1, side1set + b, ref side1vel, 1.0f);
+        side2 = Mathf.SmoothDamp(side2, side2set + b, ref side2vel, 1.0f);
+        side3 = Mathf.SmoothDamp(side3, side3set + b, ref side3vel, 1.0f);
+        side4 = Mathf.SmoothDamp(side4, side4set + b, ref side4vel, 1.0f);
+        side5 = Mathf.SmoothDamp(side5, side5set + b, ref side5vel, 1.0f);
+
+        //side0 += moveSpeed * ((side0set * a + b) - side0);
+        //side1 += moveSpeed * ((side1set * a + b) - side1);
+        //side2 += moveSpeed * ((side2set * a + b) - side2);
+        //side3 += moveSpeed * ((side3set * a + b) - side3);
+        //side4 += moveSpeed * ((side4set * a + b) - side4);
+        //side5 += moveSpeed * ((side5set * a + b) - side5);
+
+        Tetrahedron t = edge_input(side0*a+b, side1*a+b, side2*a+b, side3*a+b, side4*a+b, side5*a+b);
         alpha.transform.localPosition = t.A;
         beta.transform.localPosition = t.B;
         charlie.transform.localPosition = t.C;
